af74d07a18124001e7b9cbaaf6cedec5a15fbe62
V-Raghavendra <harishraghavendramsd7@gmail.com>
2020-10-09 08:06:49 +0530

    .

--- code3/senti..py
+++ code3/senti..py
@@ -61,7 +61,7 @@ words_freq = [(word ,sum_words[0, i]) for word, i in cv.vocabulary_.items()]
 words_freq = sorted(words_freq, key=lambda x: x[1],reverse=True)
 frequency = pd.DataFrame(words_freq, columns=['word', 'freq'])
 
-print(frequency.head(30))
+print(frequency.head(20))
 frequency.head().plot(x='word', y='freq', kind ='bar', figsize=(10, 4),color ='blue')
 plt.title("Most frequently occuring words - TOP 30")
 plt.show()
