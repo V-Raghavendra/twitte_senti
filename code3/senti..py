9e46abd7e473f8655d878f73e077e3c5dfa4984a
V-Raghavendra <harishraghavendramsd7@gmail.com>
2020-10-09 08:02:02 +0530

    display the most frequent words

--- code3/senti..py
+++ code3/senti..py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn as sks
 import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer
 #importing data
 data = pd.read_csv('train_tweet.csv')
 #checking data
@@ -25,14 +26,14 @@ print(data[data['label'] == 0].head(10))
 print(data['label'].value_counts())
 
 #performing eda on the data
-# sns.countplot(x='label' , data = data )
-#plt.show()
+sns.countplot(x='label' , data = data )
+plt.show()
 
 print(data['tweet'].str.len())
 lst=data['tweet'].str.len()
-# print("Hey" , lst)
-# sns.histplot( lst  )
-#plt.show()
+print("Hey" , lst)
+sns.histplot( lst  )
+plt.show()
 
 #adding a new coloumn to the data
 data['len'] = data['tweet'].str.len()
@@ -50,7 +51,20 @@ plt.title('variation of length')
 plt.xlabel('Length')
 plt.show()
 
-#converting the word into  word tokens
+#converting the word into to count tokens
+cv = CountVectorizer(stop_words='english')
+words = cv.fit_transform(data.tweet)
+sum_words = words.sum(axis=0)
+print(sum_words)
+#
+words_freq = [(word ,sum_words[0, i]) for word, i in cv.vocabulary_.items()]
+words_freq = sorted(words_freq, key=lambda x: x[1],reverse=True)
+frequency = pd.DataFrame(words_freq, columns=['word', 'freq'])
+
+print(frequency.head(30))
+frequency.head().plot(x='word', y='freq', kind ='bar', figsize=(10, 4),color ='blue')
+plt.title("Most frequently occuring words - TOP 30")
+plt.show()
 
 
 
