1a7df100ff2dd2e4e534c45b43bc4e281d3cbd1a
V-Raghavendra <harishraghavendramsd7@gmail.com>
2020-10-14 01:15:37 +0530

    .

--- code3/senti..py
+++ code3/senti..py
@@ -139,6 +139,14 @@ model_w2v = gensim.models.Word2Vec(tokenized_tweet, size = 200,window = 5, min_c
 model_w2v.train(tokenized_tweet,total_examples = len(data['tweet']),epochs = 20 )
 print(model_w2v)
 
+#testing the dataset using the worf classification
+test_dine = model_w2v.wv.most_similar(positive= 'dinner')
+print(test_dine)
+test_milk = model_w2v.wv.most_similar(positive =  'milk')
+print(test_milk)
+test_hate = model_w2v.wv.most_similar(negative = "hate")
+print(test_hate)
+
 
 
 
